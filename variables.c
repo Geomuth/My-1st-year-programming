7119ee54b18dda41e1778eebf01fa31cfc5c4121
geoffrey muthoka <geoffreymuthoka200@gmail.com>
2025-09-20 12:27:44 +0300

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,21 @@
+/*author geoofrey muthoka
+reg no:CT101/G/24436/24
+DATE:20/09/2024
+program:variables and data types
+*/
+#include <stdio.h>
+int main() {
+    char a = 'k';//character print is %c
+    char name[] = {"geoffrey"};//string prnt is %s
+    int age = 20;//integer print is %d
+    float pi = 3.142;//float print if %f
+    double profit = 10000.10;//double print is %lf
+    //display the results
+    printf("the character is %c \n",a);
+    printf("the (string) name is %s \n",name);
+    printf("the age is %d  \n",age);
+    printf("the pi valie is %f \n", pi);
+    printf("the profit is %lf", profit);
+    
+    return 0;
+} 
